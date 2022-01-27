a97abad167fc7a7c0e5779ac1f00ca26634daae6
Ilia Koliesnikov <bukovskii@ua.fm>
2022-01-27 16:09:21 +0300

    test-auth. 8 commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import './firebase';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from './store';
 import { Provider } from 'react-redux';
-import './firebase';
 
 ReactDOM.render(
   <>
